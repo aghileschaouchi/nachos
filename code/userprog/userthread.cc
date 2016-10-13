d5f06f38c5e58f32261f04ecb38bd126c27ac08f
Aghiles Chaouchi <achaouchi@emi.u-bordeaux1.fr>
2016-10-13 15:29:21 +0200

    On commence le TP2

---
+++ code/userprog/userthread.cc
@@ -0,0 +1,6 @@
+#ifdef CHANGED
+#include "userthread.h"
+
+int do_ThreadCreate(int f, int arg){
+}
+#endif //CHANGED
---
+++ code/userprog/userthread.h
@@ -0,0 +1,5 @@
+#ifdef CHANGED
+//#include ""
+
+extern int do_ThreadCreate(int f, int arg);
+#endif //CHANGED
